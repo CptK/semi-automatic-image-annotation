6f67d09ca8217dbde96307286bce1cc1badf49c4
Marcus Kornmann <m-kornmannmx.de>
2024-05-27 17:52:13 +0200

    refactor annotation store

--- annotator/annotation_store.py
+++ annotator/annotation_store.py
@@ -8,88 +8,55 @@ import yaml
 from PIL import Image
 
 
-class AnnotationStore:
-    """A class for storing and managing annotations.
+class SingleImage:
+    """A class for storing annotations for a single image.
 
     Args:
-        data_path: The path to the directory containing the images.
+        path: The path to the image file. (including the file name)
+        name: The file name of the image.
         model: The object detection model to use for automatic annotation.
         available_labels: A list of available class labels.
+        img_size: The size to which to resize the image for automatic annotation.
+
+    Attributes:
+        path: The path to the image file. (including the file name)
+        name: The file name of the image.
+        boxes: A list of bounding boxes in the image.
+        labels: A list of class labels corresponding to the bounding boxes.
+        ready: Whether the image has been marked as ready for export.
+        skip: Whether the image should be skipped during annotation.
+        auto_intialized: Whether the image has been automatically initialized with annotations.
+        model: The object detection model to use for automatic annotation.
+        available_labels: A list of available class labels.
+        img_size: The size to which to resize the image for automatic annotation.
     """
 
-    def __init__(self, data_path: str, model, available_labels: list[str]):
-        img_files = [f for f in os.listdir(data_path)]
+    def __init__(self, path: str, name: str, model, available_labels: list[str], img_size: tuple[int, int] | int = 640) -> None:
+        self.path = path
+        self.name = name
+        self.boxes = []
+        self.labels = []
+        self.ready = False
+        self.skip = False
+        self.auto_intialized = False
         self.model = model
-        self.data_path = data_path
         self.available_labels = available_labels
+        self.img_size = img_size if isinstance(img_size, tuple) else (img_size, img_size)
 
-        self.annotations = [
-            {"file_name": img, "boxes": [], "labels": [], "marked_ready": False, "auto_intialized": False}
-            for img in img_files
-        ]
-
-        self.current: int = 0
-        self.jump_to(self.current)
-
-    def next(self):
-        """Move to the next image in the dataset. If the end of the dataset is reached, do nothing.
-        
-        If the next image has not been seen before, initialize it with automatic annotation.
-        """
-        self.current = self.current + 1
-
-        if self.current >= len(self.annotations):
+    def init(self):
+        """Initialize the image with automatic annotation using the object detection model."""
+        if self.auto_intialized:
             return
-
-        if not self.annotations[self.current]["auto_intialized"]:
-            self.init_image()
-
-    def jump_to(self, index):
-        """Jump to a specific image in the dataset.
-
-        If the image has not been seen before, initialize it with automatic annotation.
-        
-        Args:
-            index: The index of the image to jump to.
-        """
-        self.current = index
-        if not self.annotations[self.current]["auto_intialized"]:
-            self.init_image()
-
-    def save(self):
-        raise NotImplementedError
-
-    def export(self, path: str, format: Literal["csv", "json", "yolo"], only_ready: bool, train_split: float):
-        """Export the annotations to a file in the specified format.
-
-        Args:
-            path: The path to the output file or directory.
-            format: The format in which to export the annotations (csv, json, or yolo).
-            only_ready: If True, only export images that have been marked as ready.
-            train_split: The fraction of the data to use for training in the YOLO format.
-        """
-        data = [a for a in self.annotations if not only_ready or a["marked_ready"]]
-        if format.lower() == "csv":
-            self._export_csv(path, data)
-        elif format.lower() == "json":
-            self._export_json(path, data)
-        elif format.lower() == "yolo":
-            train = data[: int(len(data) * train_split)]
-            test = data[int(len(data) * train_split) :]
-            self._export_yolo(path, train, test)
-
-    def init_image(self):
-        """Initialize the current image with automatic annotation using the object detection model."""
         if self.model is not None:
             try:
-                img = Image.open(self.file_path)
+                img = Image.open(self.path)
                 res = self._detect_single(img)
-                self.annotations[self.current]["boxes"] = [r["box"] for r in res]
-                self.annotations[self.current]["labels"] = [r["label"] for r in res]
-                self.annotations[self.current]["auto_intialized"] = True
+                self.boxes = [r["box"] for r in res]
+                self.labels = [r["label"] for r in res]
+                self.auto_intialized = True
             except Exception as e:
                 print(f"Failed to initialize image: {e}")
-
+    
     def _detect_single(self, img):
         """Detect objects in a single image and return the results as a list of dictionaries.
 
@@ -119,6 +86,97 @@ class AnnotationStore:
                 }
             )
         return res
+    
+    def mark_ready(self):
+        """Mark the image as ready for export."""
+        self.ready = True
+
+    def change_label(self, idx, label):
+        """Change the label of a bounding box in the image."""
+        self.labels[idx] = label
+
+    def delete(self, idx):
+        """Delete a bounding box from the image."""
+        self.boxes.pop(idx)
+        self.labels.pop(idx)
+
+    def add_box(self, box, label="none"):
+        """Add a bounding box to the image."""
+        self.boxes.append(box)
+        self.labels.append(label)
+    
+    def __dict__(self):
+        return {
+            "file_name": self.name,
+            "boxes": self.boxes,
+            "labels": self.labels,
+            "ready": self.ready,
+            "skip": self.skip,
+        }
+
+
+class AnnotationStore:
+    """A class for storing and managing annotations.
+
+    Args:
+        data_path: The path to the directory containing the images.
+        model: The object detection model to use for automatic annotation.
+        available_labels: A list of available class labels.
+    """
+
+    def __init__(self, data_path: str, model, available_labels: list[str]):
+        img_files = [f for f in os.listdir(data_path)]
+        self.model = model
+        self.data_path = data_path
+        self.available_labels = available_labels
+
+        self.annotations = [
+            SingleImage(os.path.join(data_path, f), f, model, available_labels, 640) for f in img_files
+        ]
+
+        self.current_index: int = 0
+        self.jump_to(self.current_index)
+
+    def next(self):
+        """Move to the next image in the dataset. If the end of the dataset is reached, do nothing.
+        
+        If the next image has not been seen before, initialize it with automatic annotation.
+        """
+        self.jump_to(max(self.current + 1, len(self.annotations) - 1))
+
+    def jump_to(self, index: int):
+        """Jump to a specific image in the dataset.
+
+        If the image has not been seen before, initialize it with automatic annotation.
+        
+        Args:
+            index: The index of the image to jump to.
+        """
+        self.current_index = index
+        if not self.current.auto_intialized:
+            self.current.init()
+
+    def save(self):
+        raise NotImplementedError
+
+    def export(self, path: str, format: Literal["csv", "json", "yolo"], only_ready: bool, train_split: float):
+        """Export the annotations to a file in the specified format.
+
+        Args:
+            path: The path to the output file or directory.
+            format: The format in which to export the annotations (csv, json, or yolo).
+            only_ready: If True, only export images that have been marked as ready.
+            train_split: The fraction of the data to use for training in the YOLO format.
+        """
+        data = [a for a in self.annotations if not only_ready or a.ready]
+        if format.lower() == "csv":
+            self._export_csv(path, data)
+        elif format.lower() == "json":
+            self._export_json(path)
+        elif format.lower() == "yolo":
+            train = data[: int(len(data) * train_split)]
+            test = data[int(len(data) * train_split) :]
+            self._export_yolo(path, train, test)
 
     def annotation_by_name(self, name):
         """Get the index of the annotation with the specified file name.
@@ -136,48 +194,55 @@ class AnnotationStore:
 
     def mark_ready(self):
         """Mark the *current* image as ready for export."""
-        self.annotations[self.current]["marked_ready"] = True
+        self.current.ready = True
 
     def change_label(self, idx, label):
         """Change the label of a bounding box in the *current* image."""
-        self.annotations[self.current]["labels"][idx] = label  # type: ignore
+        self.current.change_label(idx, label)
 
     def delete(self, idx):
         """Delete a bounding box from the *current* image."""
-        self.annotations[self.current]["boxes"].pop(idx)  # type: ignore
-        self.annotations[self.current]["labels"].pop(idx)  # type: ignore
+        self.current.delete(idx)
 
     def add_box(self, box, label="none"):
         """Add a bounding box to the *current* image."""
-        self.annotations[self.current]["boxes"].append(box)  # type: ignore
-        self.annotations[self.current]["labels"].append(label)  # type: ignore
+        self.current.add_box(box, label)
+
+    @property
+    def current(self) -> SingleImage:
+        """The index of the *current* image in the dataset."""
+        return self.annotations[self.current_index]
 
     @property
     def file_path(self):
         """The absolute file path of the *current* image."""
-        return os.path.join(self.data_path, str(self.annotations[self.current]["file_name"]))
+        return self.current.path
 
     @property
     def boxes(self):
         """The bounding boxes of the *current* image."""
-        return self.annotations[self.current]["boxes"]
+        return self.current.boxes
 
     @property
     def labels(self):
         """The class labels of the *current* image."""
-        return self.annotations[self.current]["labels"]
+        return self.current.labels
 
     @property
     def ready(self):
         """Whether the *current* image has been marked as ready for export."""
-        return self.annotations[self.current]["marked_ready"]
+        return self.current.ready
 
     @property
     def image_names(self):
         """A list of file names of all images in the dataset."""
-        return [a["file_name"] for a in self.annotations]
+        return [a.name for a in self.annotations]
+    
+    @property
+    def to_json(self):
+        return [a.__dict__() for a in self.annotations]
 
-    def _export_csv(self, path: str, data: list[dict], delimiter: str = ";"):
+    def _export_csv(self, path: str, data: list[SingleImage], delimiter: str = ";"):
         """Export the annotations to a CSV file.
 
         Args:
@@ -190,13 +255,13 @@ class AnnotationStore:
 
         with open(path, "w") as f:
             for a in data:
-                for box, label in zip(a["boxes"], a["labels"]):
+                for box, label in zip(a.boxes, a.labels):
                     x1, y1, x2, y2 = box
                     f.write(
-                        f"{a['file_name']}{delimiter}{x1}{delimiter}{y1}{delimiter}{x2}{delimiter}{y2}{delimiter}{label}\n"
+                        f"{a.name}{delimiter}{x1}{delimiter}{y1}{delimiter}{x2}{delimiter}{y2}{delimiter}{label}\n"
                     )
 
-    def _export_json(self, path: str, data: list[dict]):
+    def _export_json(self, path: str):
         """Export the annotations to a JSON file.
 
         Args:
@@ -207,9 +272,9 @@ class AnnotationStore:
             raise ValueError("Export path must be a JSON file.")
 
         with open(path, "w") as f:
-            json.dump(data, f)
+            json.dump(self.to_json, f)
 
-    def _export_yolo(self, path: str, train: list[dict], test: list[dict]):
+    def _export_yolo(self, path: str, train: list[SingleImage], test: list[SingleImage]):
         """Export the annotations to the YOLO format.
 
         Args:
@@ -226,12 +291,12 @@ class AnnotationStore:
         os.makedirs(os.path.join(path, "test", "labels"), exist_ok=True)
 
         for i, data in enumerate(train):
-            img = Image.open(os.path.join(self.data_path, data["file_name"]))
+            img = Image.open(os.path.join(self.data_path, data.name))
             img = img.resize((640, 640))
             img.save(os.path.join(path, "train", "images", f"{i}.jpg"))
 
             with open(os.path.join(path, "train", "labels", f"{i}.txt"), "w") as f:
-                for box, label in zip(data["boxes"], data["labels"]):
+                for box, label in zip(data.boxes, data.labels):
                     x1, y1, x2, y2 = box
 
                     # get the center of the box in range 0-1
@@ -244,12 +309,12 @@ class AnnotationStore:
                     f.write(f"{self.available_labels.index(label)} {x_center} {y_center} {width} {height}\n")
 
         for i, data in enumerate(test):
-            img = Image.open(os.path.join(self.data_path, data["file_name"]))
+            img = Image.open(os.path.join(self.data_path, data.name))
             img = img.resize((640, 640))
             img.save(os.path.join(path, "test", "images", f"{i}.jpg"))
 
             with open(os.path.join(path, "test", "labels", f"{i}.txt"), "w") as f:
-                for box, label in zip(data["boxes"], data["labels"]):
+                for box, label in zip(data.boxes, data.labels):
                     x1, y1, x2, y2 = box
 
                     # get the center of the box in range 0-1
